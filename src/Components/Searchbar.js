3d2df08bf5ed9e50bf4e4dbd9ba0c49a1adb19db
Domenic Sithimolada <sithimoladad@gmail.com>
2021-07-28 21:55:32 -0500

    create searchbar component, take search value and fetch api to store data

--- src/Components/Searchbar.js
+++ src/Components/Searchbar.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Searchbar = () => (
-    <form className="form">
-        <label className="label" htmlFor="query"> Cocktail</label>
-        <input className="input" type="text" name="query" placeholder="Margarita" />
-        <button className="button" type="submit">Search</button>
-    </form>
-)
+const Searchbar = () => {
+    // states for input
+    const [query, setQuery] = useState('');
+
+    // states for searched cocktails
+    const [cocktails, setCocktails] = useState([]);
+
+    const searchItems = async (e) => {
+        e.preventDefault(); // avoid page refresh
+        
+        // test query
+        //const query = "margarita";
+
+        // api url for search by cocktail name
+        const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`;
+
+        try {
+            // wait for promise to finish code
+            const res = await fetch(url);
+            const data = await res.json(); // return promise
+
+            setCocktails(data.drinks);
+            console.log(cocktails);
+        } catch(err) {
+            console.log(err);
+        }
+
+    }
+
+    return (
+        <form className="form" onSubmit={searchItems}>
+            <input className="input" type="text" name="query" 
+                placeholder="i.e. Margarita" autoComplete="off" 
+                value={query} onChange={(e) => setQuery(e.target.value)}
+                />
+            <button className="button" type="submit">Search</button>
+        </form>
+    )
+}
 
 export default Searchbar;
